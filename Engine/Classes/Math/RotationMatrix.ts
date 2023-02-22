d0ee38ea685858953ea57fe63e33dbb010f31acf
AntoineBRTL <antoine.bartoli@outlook.fr>
2023-02-22 21:58:39 +0100

    Clean rotation matrix code

--- Engine/Classes/Math/RotationMatrix.ts
+++ Engine/Classes/Math/RotationMatrix.ts
@@ -18,34 +18,34 @@ export class RotationMatrix extends Matrix4
         let cosPsi: number;
         let sinPsi: number;
 
-        radScalar = Math.PI / 180.0;
-        phi = axis.x * radScalar;
-        theta = axis.y * radScalar;
-        psi = axis.z * radScalar;
+        radScalar   = Math.PI / 180.0;
+        phi         = axis.x * radScalar;
+        theta       = axis.y * radScalar;
+        psi         = axis.z * radScalar;
 
         // TODO: OPTIMIZE SIN() COMPUTATION USING COS()
-        cosPhi = Math.cos(phi);
-        sinPhi = Math.sin(phi);
-        cosTheta = Math.cos(theta);
-        sinTheta = Math.sin(theta);
-        cosPsi = Math.cos(psi);
-        sinPsi = Math.sin(psi);
+        cosPhi      = Math.cos(phi);
+        sinPhi      = Math.sin(phi);
+        cosTheta    = Math.cos(theta);
+        sinTheta    = Math.sin(theta);
+        cosPsi      = Math.cos(psi);
+        sinPsi      = Math.sin(psi);
 
-        this[0] = cosTheta * cosPsi;
-        this[1] = -cosPhi * sinPsi + sinPhi * sinTheta * cosPsi;
-        this[2] = sinPhi * sinPsi + cosPhi * sinTheta * cosPsi;
-        this[3] = 0;
-        this[4] = cosTheta * sinPsi;
-        this[5] = cosPhi * cosPsi + sinPhi * sinTheta * sinPsi;
-        this[6] = -sinPhi * cosPsi + cosPhi * sinTheta * sinPsi;
-        this[7] = 0;
-        this[8] = -sinTheta;
-        this[9] = sinPhi * cosTheta;
-        this[10] = cosPhi * cosTheta;
-        this[11] = 0;
-        this[12] = 0;
-        this[13] = 0;
-        this[14] = 0;
-        this[15] = 1;
+        this[0]     = cosTheta * cosPsi;
+        this[1]     = -cosPhi * sinPsi + sinPhi * sinTheta * cosPsi;
+        this[2]     = sinPhi * sinPsi + cosPhi * sinTheta * cosPsi;
+        this[3]     = 0;
+        this[4]     = cosTheta * sinPsi;
+        this[5]     = cosPhi * cosPsi + sinPhi * sinTheta * sinPsi;
+        this[6]     = -sinPhi * cosPsi + cosPhi * sinTheta * sinPsi;
+        this[7]     = 0;
+        this[8]     = -sinTheta;
+        this[9]     = sinPhi * cosTheta;
+        this[10]    = cosPhi * cosTheta;
+        this[11]    = 0;
+        this[12]    = 0;
+        this[13]    = 0;
+        this[14]    = 0;
+        this[15]    = 1;
     }
 }
