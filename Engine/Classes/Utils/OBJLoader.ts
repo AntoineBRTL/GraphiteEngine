88019beeb2c49b4d7f45f4c8d796de065479ae90
AntoineBRTL <antoine.bartoli@outlook.fr>
2023-02-22 22:29:42 +0100

    Optimisation

--- Engine/Classes/Utils/OBJLoader.ts
+++ Engine/Classes/Utils/OBJLoader.ts
@@ -74,41 +74,33 @@ export class OBJLoader{
 
                 if(name == "f")
                 {
-                    function setIndice(indices: number[])
+                    function setIndice(indices: string[])
                     {
-                        let vi = indices[0] - 1;
-                        let ti = indices[1] - 1;
-                        let ni = indices[2] - 1;
+                        let vi = parseInt(indices[0]) - 1;
+                        let ti = parseInt(indices[1]) - 1;
+                        let ni = parseInt(indices[2]) - 1;
 
                         mesh.addVertexAsNumbers(...vertices[vi]);
                         mesh.addVertexAsNumbers(...uvs[ti]);
                         mesh.addVertexAsNumbers(...normals[ni]);
                     }
                     
-                    let indices_0: number[];
-                    let indices_1: number[];
-                    let indices_2: number[];
+                    let indices_0: string[];
+                    let indices_1: string[];
+                    let indices_2: string[];
 
-                    function toNumberArray(indices: string[])
-                    {
-                        let intIndices: number[];
-                        intIndices = new Array(indices.length);
-                        for(let i = 0; i < indices.length; i++) intIndices[i] = parseInt(indices[i]);
-                        return intIndices;
-                    }
-
-                    indices_0 = toNumberArray(splitedLine[0].split("/"));
-                    indices_1 = toNumberArray(splitedLine[1].split("/"));
-                    indices_2 = toNumberArray(splitedLine[2].split("/"));
+                    indices_0 = splitedLine[0].split("/");
+                    indices_1 = splitedLine[1].split("/");
+                    indices_2 = splitedLine[2].split("/");
 
                     setIndice(indices_0);
                     setIndice(indices_1);
                     setIndice(indices_2);
 
                     if(splitedLine.length < 4) return;
-                    
-                    let indices_3: number[];
-                    indices_3 = toNumberArray(splitedLine[3].split("/"));
+
+                    let indices_3: string[];
+                    indices_3 = splitedLine[3].split("/");
 
                     setIndice(indices_0);
                     setIndice(indices_3);
