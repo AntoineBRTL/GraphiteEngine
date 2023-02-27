562e49212459ed198f6bd9ef34e4f84a0e6247ab
AntoineBRTL <antoine.bartoli@outlook.fr>
2023-02-27 22:41:55 +0100

    Adding a new type of primitive sphere

--- Engine/Classes/Render/Sky.ts
+++ Engine/Classes/Render/Sky.ts
@@ -9,8 +9,7 @@ let vertexShader = `
 struct VertexOutput 
 {
     @builtin(position) vertPosition : vec4<f32>,
-    @location(0) fragUV : vec2<f32>,
-    @location(1) fragNormal: vec4<f32>
+    @location(0) fragUV : vec2<f32>
 };
 
 @vertex
@@ -19,17 +18,58 @@ fn main(@location(0) position: vec3<f32>, @location(1) uv: vec2<f32>, @location(
     var output : VertexOutput;
     output.vertPosition = mProj * mViewRot * vec4<f32>(position.x, position.y, position.z, 1.0);
     output.fragUV = uv;
-    output.fragNormal = normalize(vec4<f32>(normal, 1.0));
     return output;
 }`;
 
 let fragmentShader = `
 @fragment
-fn main(@location(0) uv: vec2<f32>, @location(1) normal: vec4<f32>) -> @location(0) vec4<f32>
+
+fn main(@location(0) uv: vec2<f32>) -> @location(0) vec4<f32>
 {
-    let t: f32 = uv.x;
-    let color: vec3<f32> = (1.0 - t)*vec3<f32>(1.0, 1.0, 1.0) + t*vec3<f32>(0.5, 0.7, 1.0);
+    var t: f32 = uv.x;
+    var color: vec3<f32> = (1.0 - t)*vec3<f32>(1.0, 1.0, 1.0) + t*vec3<f32>(0.5, 0.7, 1.0);
+    // color += fbm(vec2<f32>(uv.x, 1.0)) * 0.1;
     return vec4<f32>(color, 1.0);
+}
+
+fn random(st: vec2<f32>) -> f32
+{
+    return fract(sin(dot(st.xy,
+                         vec2<f32>(12.9898,78.233)))*
+        43758.5453123);
+}
+
+fn noise(st: vec2<f32>) -> f32 
+{
+    var i: vec2<f32> = floor(st);
+    var f: vec2<f32> = fract(st);
+
+    var a: f32 = random(i);
+    var b: f32 = random(i + vec2<f32>(1.0, 0.0));
+    var c: f32 = random(i + vec2<f32>(0.0, 1.0));
+    var d: f32 = random(i + vec2<f32>(1.0, 1.0));
+
+    var u: vec2<f32> = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+fn fbm(st: vec2<f32>) -> f32
+{
+    var stm: vec2<f32> = st;
+    var value: f32 = 0.0;
+    var amplitude: f32 = 0.5;
+    var frequency: f32 = 0.0;
+
+    for (var i: i32 = 0; i < 1; i++) 
+    {
+        value += amplitude * noise(stm);
+        stm *= 2.0;
+        amplitude *= 0.5;
+    }
+    return value;
 }`;
 
 export class Sky
@@ -49,14 +89,14 @@ export class Sky
         let vertexBuffer: GPUBuffer;
         let mViewRotBuffer: GPUBuffer;
         let mProjBuffer: GPUBuffer;
-        let uniformBindingGroup: GPUBindGroup;
+        let uniformBindingGroup0: GPUBindGroup;
 
         pipeline                = this.material.getRenderPipeline(camera.getRenderer());
         vertexBuffer            = this.mesh.getVertexBuffer(device);
         mViewRotBuffer          = camera.getRenderer().toUniformGPUBuffer(device, camera.getTransform().getRotationMatrix());
         mProjBuffer             = camera.getProjectionMatrixBuffer(device);
-        uniformBindingGroup     = camera.getRenderer().setupUniformBindGroup(device, pipeline, 0, mViewRotBuffer, mProjBuffer);
+        uniformBindingGroup0    = camera.getRenderer().setupUniformBindGroup(device, pipeline, 0, mViewRotBuffer, mProjBuffer);
 
-        camera.getRenderer().draw(passEncoder, pipeline, this.mesh, vertexBuffer, uniformBindingGroup);
+        camera.getRenderer().draw(passEncoder, pipeline, this.mesh, vertexBuffer, uniformBindingGroup0);
     }
 }
